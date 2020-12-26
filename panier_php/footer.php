b1edfad183c9cd626c280e6be27e0ef4e227e5a2
IBEGHOUCHENE <ibeghouchene.nadir@gmail.com>
2020-12-26 22:15:49 +0100

    Panier en PHP remis à nouveau

---
+++ panier_php/footer.php
@@ -0,0 +1,13 @@
+
+
+<!--  BODY  -->
+<div id="footer">
+    <div class="wrap">
+    <p>o Shoppi, a smail eccomrce template . Handcreates by Jeremy Jones</p>
+    </div>
+</div>
+<script
+        src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script type="text/javascript" src="script.js"></script>
+</body>
+</html>
